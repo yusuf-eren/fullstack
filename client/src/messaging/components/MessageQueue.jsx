405fa9d0a34d9775b92c27cf7ce2dc1350bde342
yusuf-eren <erenyusuf170@gmail.com>
2022-12-08 14:54:10 +0300

    send time with hours:minutes

--- client/src/messaging/components/MessageQueue.jsx
+++ client/src/messaging/components/MessageQueue.jsx
@@ -1,17 +1,21 @@
 function MessageQueue({ message, sender, date }) {
 
+    const d = new Date(date);
+
     let botResponse = (<div className="item">
         <div className="icon">
             <i className="fa fa-user"></i>
         </div>
         <div className="msg">
             <p>{message}</p>
+            <span className="send-time">{`${d.getHours()}:${d.getMinutes()}`}</span>
         </div>
     </div>)
 
     let clientResponse = (<div className="item right">
         <div className="msg">
             <p>{message}</p>
+            <span className="send-time-right">{`${d.getHours()}:${d.getMinutes()}`}</span>
         </div>
     </div>)
     return (sender === 'echoBot') ? botResponse : clientResponse
